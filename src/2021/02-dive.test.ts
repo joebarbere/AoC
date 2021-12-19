d9652b6548a27d3aef17fbba43270c8d5e4cdbe3
Joe Barbere <joe.barbere@gmail.com>
2021-12-19 00:30:04 -0500

    Updated unit tests for day 02 dive

--- src/2021/02-dive.test.ts
+++ src/2021/02-dive.test.ts
@@ -4,6 +4,10 @@ describe('getNumber function', () => {
   it('returns XXX given input', () => {
     expect(getNumber('forward 5\ndown 5\nforward 8')).toBe(5 * 13);
   });
+
+  it('does not crash on bad direction', () => {
+    expect(getNumber('forward 5\ndown 5\nfoo 8')).toBe(5 * 5);
+  });
 });
 
 describe('run02_01', () => {
