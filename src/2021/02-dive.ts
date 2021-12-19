4ba52ca7a6e00114f896a31f370f543fc914317a
Joe Barbere <joe.barbere@gmail.com>
2021-12-19 00:21:58 -0500

    Finished part 01 of day 02

--- src/2021/02-dive.ts
+++ src/2021/02-dive.ts
@@ -1,12 +1,38 @@
 import {logger, logName} from '../logging';
 import {load} from '../loader';
 
-const childLogger = logger.child({name: `${logName} 01-dive`});
+const childLogger = logger.child({name: `${logName} 02-dive`});
 
 export function getNumber(input: string): number {
   const lines: string[] = input.split('\n');
   childLogger.info(lines);
-  return 0;
+  const data = lines.map(val => {
+    return val.split(' ');
+  });
+  childLogger.info(data);
+  let horizontalPosition = 0;
+  let depth = 0;
+  data.forEach((value, _) => {
+    const direction = value[0];
+    const amount = Number(value[1]);
+    switch (direction) {
+      case 'forward':
+        horizontalPosition += amount;
+        break;
+      case 'up':
+        depth -= amount;
+        break;
+      case 'down':
+        depth += amount;
+        break;
+      case '':
+        break;
+      default:
+        childLogger.error(`direction unknown: ${direction}`);
+        break;
+    }
+  });
+  return horizontalPosition * depth;
 }
 
 export function run02_01(filename: string): boolean {
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {logger} from './logging';
 import {run01_01, run01_02} from './2021/01-sonar-sweep';
+import {run02_01} from './2021/02-dive';
 
 logger.info('Start!');
-run01_01('/day01.txt');
-run01_02('/day01.txt');
+//run01_01('/day01.txt');
+//run01_02('/day01.txt');
+run02_01('/day02.txt');
